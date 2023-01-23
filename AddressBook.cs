0bcf8cc6e9a8e2f97b9f927ed4b180d286906078
Dhavalsinh Raj <rdjcoding@gmail.com>
2023-01-23 16:23:49 +0530

    addressBookSystem

--- AddressBook.cs
+++ AddressBook.cs
@@ -1,17 +1,24 @@
+using System.Security.Cryptography;
+
 namespace MyApp
 {
     //add,edit,delete
     class AddressBook
     {
         Dictionary<string,Person> name2Person;
-
-        public AddressBook()
+        public string name;
+        public AddressBook(string name)
         {
+            this.name = name;
             name2Person = new Dictionary<string,Person>();
         }
 
         public void AddPerson(Person p)
         {
+            if(name2Person.ContainsKey(p.firstname+p.lastname))
+            {
+                throw new Exception("person already exists");
+            }
             name2Person[p.firstname+p.lastname] = p;
         }
 
@@ -83,14 +90,81 @@ namespace MyApp
             System.Console.WriteLine("Updated!");
         }
 
+        public void displayByCity(string city)
+        {
+            Console.WriteLine("Persons with city = "+city + " in book: "+name+"\n---------------");
+            foreach(var p in name2Person)
+            {
+                if(p.Value.city==city)
+                {
+                    Console.WriteLine(p.Value.ToString());
+                }
+            }
+        }
+
+        public IEnumerable<Person> SearchByCity(string fname,string city)
+        {
+            IEnumerable<Person> p = from kvp in name2Person
+                                    where kvp.Value.city == city && kvp.Value.firstname==fname
+                                    select kvp.Value;
+            return p;
+        }
+
+        public IEnumerable<Person> GetAllPersons()
+        {
+            return name2Person.Values;
+        }
+
         public override string ToString()
         {
-            string ret = "";
+            string ret = "------------------\n\t"+name+"\n--------------------";
             foreach(var kvp in name2Person)
             {
                 ret+= "\n\n---------------------\n"+ kvp.Value.ToString();
             }
             return ret;
+        }
+    }
+
+    class AddressBookSystem
+    {
+        Dictionary<string, AddressBook> name2book;
+        public AddressBookSystem() { 
+            name2book = new Dictionary<string, AddressBook>();
+        }
+
+        public void AddBook(AddressBook ab)
+        {
+            name2book[ab.name] = ab;
+        }
+
+        public void displayByCity(string city)
+        {
+            foreach(var kvp in name2book)
+            {
+                kvp.Value.displayByCity(city);
+            }
+        }
+
+        public void SearchByCity(string fname,string city)
+        {
+            Console.WriteLine($"Persons with firstname = {fname} and city = {city} \n---------------");
+            foreach(var kvp in name2book)
+            {
+                foreach(Person p in kvp.Value.SearchByCity(fname,city))
+                {
+                    Console.WriteLine(p.ToString());
+                }
+            }
+        }
+
+        public int CountByCity(string city)
+        {
+            int c = (from book in name2book.Values
+                     from person in book.GetAllPersons()
+                     where person.city == city
+                     select person).Count();
+            return c;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,22 +6,30 @@
         {
             Person p1 = new Person("John","wick","121,Mill Neck,Long Island,NY","Long Island","New York","12345","1235566","Johnwick@gmail.com");
             Person p2 = new Person("james","wick","122,Mill Neck,Long Island,NY","Long Island","New York","12345","1235566","Johnwick@gmail.com");
-            AddressBook ab = new AddressBook();
+            Person pp = new Person("Sherlock", "wicks", "21,some", "London", "England", "2131", "1323214", "sherlockwicks@gmail.com");
+            AddressBook ab = new AddressBook("Book 1");
             ab.AddPerson(p1);
             ab.AddPerson(p2);
+            ab.AddPerson(pp);
             System.Console.WriteLine(ab.ToString());
-            System.Console.WriteLine("Deleting james wick");
-            ab.DeletePerson("james","wick");
-            System.Console.WriteLine(ab.ToString());
-            System.Console.WriteLine("For editing :\nEnter firstname");
-            string fs = Console.ReadLine();
-            System.Console.WriteLine("Enter lastname");
-            string ls = Console.ReadLine();
-            ab.EditPerson(fs,ls);
 
-            System.Console.WriteLine("\n\n updated address book");
-            System.Console.WriteLine(ab.ToString());
-            
+            Person p3 = new Person("Sherlock", "holmes", "221B,Backer street", "London", "England", "1233", "930130431", "benb@gnail.com");
+            Person p4 = new Person("John", "watson", "221A,Macker street", "London", "England", "1232123", "540130431", "benb1@gnail.com");
+            AddressBook ab2 = new AddressBook("Book 2");
+            ab2.AddPerson(p3);
+            ab2.AddPerson(p4);
+            Console.WriteLine(ab2.ToString());
+
+            //uc6
+            AddressBookSystem abs = new AddressBookSystem();
+            abs.AddBook(ab);
+            abs.AddBook(ab2);
+            //uc8
+            abs.displayByCity("London");
+            //uc9
+            abs.SearchByCity("Sherlock", "London");
+            //uc10
+            Console.WriteLine("Count of person with city=London is " + abs.CountByCity("London"));
         }
     }
 }
